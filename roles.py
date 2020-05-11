f1fdd50cdaf8783f86af2d7209c2e8be7b293e62
Hunterfollow123 <49564969+Hunterfollow123@users.noreply.github.com>
2020-05-11 14:10:47 +0100

    Update roles.py

--- roles.py
+++ roles.py
@@ -6,40 +6,60 @@ import random, os, time
 
 os.system("cls")
 print("\n")
-
 path = str(input("Path to the players file : "))
-os.system("clear")
-print("\n")
-file = open(path, "r").readlines()
+def main():
+    os.system("cls")
+    print("\n")
+
+    os.system("cls")
+
+    print("\n")
+
+    file = open(path, "r").readlines()
 
 
-players = list(random.sample(file, k=len(file)))
+    players = list(random.sample(file, k=len(file)))
+
+    try:
+        wolfs_num = int(input("How many wolfs ? : "))
+    
+    except:
+        print("[!] invalid, please enter a number")
+        wolfs_num = int(input("How many wolfs ? : "))
+    
+    print("\n" + str(len(file)), "players,", wolfs_num, "wolfs,", "6 roles,", len(file) - wolfs_num - 6, "Villagers\n")
 
 
-s = 0
-for player in players:
-    if player == players[2]:
-        break
-    s = s + 1
-    print("wolf", s, " : ", player, end="\r")
-    players.remove(player)
-    time.sleep(0.2)
+    s = 0
+
+    for player in players:
+        try:
+            if player == players[wolfs_num]:
+                break
+        except:
+            print("too many wolfs!")
+            exit()
+        
+        s = s + 1
+        print("wolf", s, " : ", player, end="\r")
+        players.remove(player)
+        time.sleep(0.2)
 
 
-print("\n")
+    print("\n")
 
-s = 0
-for player in players:
-    if player == players[6]:
-        break
-    s = s + 1
-    print("role", s, ":", player, end="\r")
-    players.remove(player)
-    time.sleep(0.2)
+    s = 0
+    for player in players:
+        if player == players[6]:
+            break
+        s = s + 1
+        print("role", s, ":", player, end="\r")
+        players.remove(player)
+        time.sleep(0.2)
 
-print("\n")
+    print("\n")
 
-print("""role 1 = seeker
+    print("""role 1 = seeker
 role 2 = hunter
 role 3 = witch
 role 4 = salva
@@ -47,4 +67,69 @@ role 5 = crow
 role 6 = sniper
 """)
 
+def rerun():
+    os.system("cls")
+    print("\n")
 
+    file = open(path, "r").readlines()
+
+
+    players = list(random.sample(file, k=len(file)))
+
+    try:
+        wolfs_num = int(input("How many wolfs ? : "))
+    
+    except:
+        print("[!] invalid, please enter a number")
+        wolfs_num = int(input("How many wolfs ? : "))
+    
+    print("\n" + str(len(file)), "players,", wolfs_num, "wolfs,", "6 roles,", len(file) - wolfs_num - 6, "Villagers\n")
+
+
+    s = 0
+
+    for player in players:
+        try:
+            if player == players[wolfs_num]:
+                break
+        except:
+            print("too many wolfs!")
+            exit()
+        
+        s = s + 1
+        print("wolf", s, " : ", player, end="\r")
+        players.remove(player)
+        time.sleep(0.2)
+
+
+    print("\n")
+
+    s = 0
+    for player in players:
+        if player == players[6]:
+            break
+        s = s + 1
+        print("role", s, ":", player, end="\r")
+        players.remove(player)
+        time.sleep(0.2)
+
+    print("\n")
+
+    print("""role 1 = seeker
+role 2 = hunter
+role 3 = witch
+role 4 = salva
+role 5 = crow
+role 6 = sniper
+""")
+
+main()
+while True:
+    choice = str(input("\n[0]exit    [1]re-run\n\n>> : "))
+    if choice == "0":
+        exit()
+    elif choice == "1":
+        rerun()
+    else:
+        print("[!]invalid")
+        
